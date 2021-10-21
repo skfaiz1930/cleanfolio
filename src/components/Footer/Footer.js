349ca77c6f44cc71a25fc5270ef2eedd421f93c4
skfaiz1930 <61198181+skfaiz1930@users.noreply.github.com>
2021-10-21 20:46:44 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rajshekhar26/cleanfolio'
+      href='https://github.com/skfaiz1930/cleanfolio'
       className='link footer__link'
     >
-      Created By Raj Shekhar
+      Created By Shaikh Faizan
     </a>
   </footer>
 )
